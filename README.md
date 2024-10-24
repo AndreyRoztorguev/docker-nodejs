8fcf03dfe3b49338b50f1f24ee34a764bdc7a2a7
Andrey Roztorguev <andreyroztorguev@MacBookAir.net>
2024-10-24 18:26:31 +0300

    initial

---
+++ README.md
@@ -0,0 +1,17 @@
+# Sample Node.js application
+
+This repository is a sample Node.js application for Docker's documentation.
+
+## create .env
+
+1. SERVER_PORT=8080 (should be identical to the Dockerfile EXPOSE 8080) REQUIRED!
+2. POSTGRES_HOST=localhost (not required!)
+3. POSTGRES_USER=postgres (not required!)
+4. POSTGRES_PASSWORD=postgres (not required!)
+5. POSTGRES_DB=docker-nodejs-db (not required!)
+
+## Start docker compose
+
+```
+docker compose  up --build
+```
---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const db = require('./persistence');
+const getItems = require('./routes/getItems');
+const addItem = require('./routes/addItem');
+const updateItem = require('./routes/updateItem');
+const deleteItem = require('./routes/deleteItem');
+
+const server_port = process.env.SERVER_PORT || 3000;
+
+app.use(express.json());
+app.use(express.static(__dirname + '/static'));
+
+app.get('/items', getItems);
+app.post('/items', addItem);
+app.put('/items/:id', updateItem);
+app.delete('/items/:id', deleteItem);
+
+db.init()
+    .then(() => {
+        app.listen(server_port, () =>
+            console.log(`Listening on port ${server_port}`),
+        );
+    })
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
+
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit());
+};
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
